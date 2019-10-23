7d576d3be8462d795620fe3bcb618171dc20a630
DupinderSingh <dupinder.15c@gmail.com>
2019-10-23 17:44:06 +0530

    brmief intro

--- src/app/store/effects/config.effects.ts
+++ src/app/store/effects/config.effects.ts
@@ -8,6 +8,14 @@ import { IConfig } from '../../models/config.interface';
 import { ConfigService } from './../../services/config.service';
 import { EConfigActions, GetConfig, GetConfigSuccess } from '../actions/config.actions';
 
+/**
+ * Effect responsible for deciding if there any change in the action.
+ * switchMap will cancel all the previous request if there and get the latest request response.
+ * of returns whatever it takes.
+ * ofType returns the observable of action. "For example, if actions has type Actions<AdditionAction|SubstractionAction>, and the type of the Addition action is add, then actions.pipe(ofType('add')) returns an Observable<AdditionAction>".
+ *
+ */
+
 @Injectable()
 export class ConfigEffects {
   @Effect()
